734b5796e9d67d5b4bc025ba83c1020518add68b
psychoScaler <146501857+psychoScaler@users.noreply.github.com>
2025-02-13 16:25:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy! <3 ILOVEYOUUUUU!!!";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -44,7 +44,7 @@ function generateMessage(noCount) {
   const messages = [
     "No",
     "Why no? :(",
-    "Bebuuu please",
+    "Mahal, please? :(((",
     "Are you sure?:(",
     "Sakit mo hays",
     "Iiyak aq d2...",
